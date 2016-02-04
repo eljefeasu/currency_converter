a00c64719730a041f34239355deb415bffe61fbf
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-04 14:39:47 -0500

    Currency objects with the same currecy code can be subtracted

--- main.rb
+++ main.rb
@@ -3,9 +3,13 @@ require './currency_converter'
 
 
 usd1 = Currency.new(10, "USD")
-usd2 = Currency.new(10, "YEN")
+usd2 = Currency.new(20, "USD")
+usd3 = Currency.new(10, "USD")
+yen1 = Currency.new(10, "YEN")
 
 puts "#{usd1.amount} #{usd1.currency_code}"
 puts "#{usd2.amount} #{usd2.currency_code}"
 
-usd1 + usd2
+usd1 - usd2
+usd1 - usd3
+usd1 - yen1
