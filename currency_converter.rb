ff14369586fbb91d954b7238cb430e98ced07e95
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-05 11:26:26 -0500

    Added functionality to raise UnknownCurrencyCodeError to CurrencyConverter class

--- currency_converter.rb
+++ currency_converter.rb
@@ -8,8 +8,21 @@ class CurrencyConverter
   end
 
   def convert(currency, currency_code)
-    converted_amount = currency.amount * (@currency_rates[currency_code].to_f)
-    return Currency.new(converted_amount, currency_code)
+    if @currency_rates.has_key?(currency_code) && @currency_rates.has_key?(currency.currency_code)
+      if currency.amount == :USD
+        converted_amount = currency.amount * (@currency_rates[currency_code].to_f)
+      else
+        converted_amount = @currency_rates[currency_code] / @currency_rates[currency.currency_code]
+      end
+      return Currency.new(converted_amount, currency_code)
+    else
+      raise UnknownCurrencyCodeError
+    end
   end
+end
 
+class UnknownCurrencyCodeError < StandardError
+  def initialize(msg='Unknown currency code used in conversion')
+    super
+  end
 end
--- main.rb
+++ main.rb
@@ -2,37 +2,43 @@ require './currency'
 require './currency_converter'
 
 
-usd1 = Currency.new(10, :USD)
+usd1 = Currency.new(1, :USD)
 usd2 = Currency.new(20, :USD)
 usd3 = Currency.new(10, :USD)
-yen1 = Currency.new(10, :YEN)
+yen1 = Currency.new(10, :JPN)
+eur1 = Currency.new(1, :EUR)
 
 currency_rates = {USD: 1.0,
-                  EUR: 0.89,
+                  EUR: 0.74,
                   GBP: 0.68,
                   INR: 67.54,
                   AUD: 1.39,
                   CAD: 1.37,
                   ZAR: 15.89,
                   NZD: 1.49,
-                  JPY: 116.71}
+                  JPY: 120.0}
 
-puts "#{usd1.amount} #{usd1.currency_code}"
-puts "#{usd2.amount} #{usd2.currency_code}"
-
-puts usd1 == usd3
-puts usd2 == usd3
-
-usd1 - usd2
-usd1 + usd3
-puts usd1.amount * 2.5
-
-usd4 = usd1 * 4.5
-puts usd4.amount
-puts usd4.currency_code
+# puts "#{usd1.amount} #{usd1.currency_code}"
+# puts "#{usd2.amount} #{usd2.currency_code}"
+#
+# puts usd1 == usd3
+# puts usd2 == usd3
+#
+# usd1 - usd2
+# usd1 + usd3
+# puts usd1.amount * 2.5
+#
+# usd4 = usd1 * 4.5
+# puts usd4.amount
+# puts usd4.currency_code
 
 currency_converter = CurrencyConverter.new(currency_rates)
-currency_converter.currency_rates
 
 converted_currency = currency_converter.convert(Currency.new(1, :USD), :EUR)
+puts "#{converted_currency.amount} #{converted_currency.currency_code}"
+
+converted_currency = currency_converter.convert(Currency.new(1, :EUR), :JPY)
+puts "#{converted_currency.amount} #{converted_currency.currency_code}"
+
+converted_currency = currency_converter.convert(Currency.new(1, :EUR), :RAF)
 puts "#{converted_currency.amount} #{converted_currency.currency_code}"
