64e4b2b28f08d21cbc610d2b7aa8979743315c6b
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-06 12:42:01 -0500

    Edited best_currency_investment method in CurrencyTrader class to compare USD to each other currency and back to USD and to output which would give the highest return

--- currency_trader.rb
+++ currency_trader.rb
@@ -9,13 +9,23 @@ class CurrencyTrader
     starting_currency = @currency_o.currency_code
     starting_amount = @currency_o.amount
     array = []
+    converted_hash = {}
 
     @old_rates_hash.each do |key, value|
       new_value = starting_amount * value
       array << new_value
     end
 
-    puts "#{array}"
+    counter = 0
+
+    @new_rates_hash.each do |key, value|
+      amount = (1/value) * array[counter]
+      converted_hash[key] = amount
+      counter += 1
+    end
+
+    max = converted_hash.key(converted_hash.values.max)
+    puts "The best investment would be #{max} with a USD value of #{converted_hash[max].round(2)}"
 
   end
 end
